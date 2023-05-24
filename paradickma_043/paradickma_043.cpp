c666e77690e7109b5d7f15b3917a44912b1d69f6
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-24 19:32:31 +0700

    commit 2

--- paradickma_043/paradickma_043.cpp
+++ paradickma_043/paradickma_043.cpp
@@ -4,3 +4,16 @@ using namespace std;
 
 class orang {
 public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "Orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "Orang dihapus\n" << endl;
+	}
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+};
