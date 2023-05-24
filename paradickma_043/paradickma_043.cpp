138f6101b69a8508882106757a3be12cde7707ed
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-24 19:33:13 +0700

    commit 3

--- paradickma_043/paradickma_043.cpp
+++ paradickma_043/paradickma_043.cpp
@@ -17,3 +17,20 @@ public:
 		return a + b;
 	}
 };
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya" + nama + "dari sekolah" + sekolah + "\n\n";
+	}
+};
